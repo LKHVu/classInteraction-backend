2f320163ac9ca9daf8686c0846aa7ce837da40cf
Vu Le <vule1128@gmail.com>
2020-01-20 06:10:39 +0700

    Admin can now upload photo of the student

--- src/main/java/vgu/vgu/Application.java
+++ src/main/java/vgu/vgu/Application.java
@@ -7,8 +7,12 @@ package vgu.vgu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+    FileStorageProperties.class
+})
 public class Application {
 
     public static void main(String[] args) {
